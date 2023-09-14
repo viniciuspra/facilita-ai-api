4f0fd2a574c112664f4f5af2e66011ab36556a84
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-09-14 15:28:29 -0300

    feat: Add CORS to server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
@@ -6,6 +7,9 @@ import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify()
 
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)

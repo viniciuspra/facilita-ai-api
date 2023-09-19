5fabad194ffe6c90056d47c27e74a8bcf82a2d07
Vinicius Cascaes Pra <vinicius.cascaesp@gmail.com>
2023-09-19 19:07:34 -0300

    .

--- build/server.js
+++ build/server.js
@@ -8,16 +8,14 @@ const create_transcription_1 = require("./routes/create-transcription");
 const generate_ai_completion_1 = require("./routes/generate-ai-completion");
 const app = (0, fastify_1.fastify)();
 app.register(cors_1.fastifyCors, {
-  origin: "*",
+    origin: '*',
 });
 app.register(get_all_prompts_1.getAllPromptsRoute);
 app.register(upload_video_1.uploadVideoRoute);
 app.register(create_transcription_1.createTranscriptionRoute);
 app.register(generate_ai_completion_1.generateAiCompletionRoute);
-app.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  console.log(`server listening on ${address}`);
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP Server Running!');
 });
